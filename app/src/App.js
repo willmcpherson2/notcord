788ce61a2f39b15a83b6b033b9415cf5692586bb
Josh Thiele <s3717991@student.rmit.edu.au>
2021-03-09 11:30:10 +1100

    installed boostrap

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to NotCord</h1>
+      <Button>Bootstrap Button</Button>
     </div>
   );
 }

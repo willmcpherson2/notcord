143986839aea660dbdf2e3296755d4c82409bfae
William McPherson <willmcpherson2@gmail.com>
2021-03-23 15:51:01 +1100

    Add database and POST request for signup
    You'll need the `sqlite3` command installed on your development machine.
    It might already be installed.
    
    On launch, we create the file `database.db`. If the file already exists,
    we use the existing one.
    
    We then add a `User` table where each entry has a `username` and
    `password_hash`. If the table already exists, we use the existing one.
    
    To sign up, the client sends a POST request to /signup with JSON data in
    the following form:
    
        {
            "username": "bob",
            "password_hash": 123
        }
    
    This will insert a User with those exact fields.
    
    Keep in mind that this is just a first version. This isn't necessarily
    secure and we also don't check if the user already exists (it will just
    insert a new user with the same name).

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 
 use rocket::response::NamedFile;
+use rocket_contrib::databases::rusqlite;
+use rocket_contrib::json::Json;
+use serde::Deserialize;
 use std::io;
 use std::path::{Path, PathBuf};
+
+#[database("database")]
+struct Database(rusqlite::Connection);
+
+#[derive(Deserialize)]
+struct Signup {
+    username: String,
+    password_hash: i64,
+}
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
@@ -16,6 +30,32 @@ fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("app/build").join(file)).ok()
 }
 
+#[post("/signup", data = "<signup>")]
+fn signup(signup: Json<Signup>, database: Database) {
+    // TODO: prevent duplicate usernames
+    database
+        .execute(
+            "INSERT INTO User (username, password_hash) VALUES (?1, ?2)",
+            &[&signup.username, &signup.password_hash],
+        )
+        .expect("bug: failed to insert user");
+}
+
+fn init_database_file() {
+    rusqlite::Connection::open("database.db")
+        .expect("bug: failed to open/create database file")
+        .execute(
+            "CREATE TABLE IF NOT EXISTS User (username TEXT NOT NULL, password_hash INTEGER)",
+            &[],
+        )
+        .expect("bug: failed to create sqlite table");
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, files]).launch();
+    init_database_file();
+
+    rocket::ignite()
+        .attach(Database::fairing())
+        .mount("/", routes![index, files, signup])
+        .launch();
 }

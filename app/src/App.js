a975f49bcbdcf4bd58e4ccfe74bdcbf2738637ab
minimuscle <joshthiele@live.com.au>
2021-03-24 14:38:33 +1100

    added login form.

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import {Button, Container, Row, Col} from 'react-bootstrap';
-import Sidebar from './Components/Sidebar/Sidebar'
-import Login from './Components/Login/Login'
+import {Button, Container} from 'react-bootstrap';
+import React, {Component} from 'react';
+import Login from './Components/Login/Login';
+import Sidebar from './Components/Sidebar/Sidebar';
+
+export default class App extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      userLoggedIn: false
+    }
+  }
+
+  renderComponents() {
+    if (!this.state.userLoggedIn) {
+      return <Login/>
+    } else {
+      return <Sidebar/>
+    }
+    
+    
+  }
 
 
-function App() {
-  //Displays the main page with the sidebar
-  return (
-      <Container fluid className="nopadding">
-        <Row>
-          <Col md="auto"><Sidebar></Sidebar></Col>
-          <Col><Login></Login></Col>
-        </Row>
-      </Container>       
-  );
+    render(){
+    return (
+      <Container fluid>
+          {this.renderComponents()}
+      </Container>
+    );
+  }
 }
-
-export default App;
+  

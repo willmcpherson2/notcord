4b1a516a56f8d106b9e7994e5834e9770a422b5f
Kaidin Jackson <s3604690@student.rmit.edu.au>
2021-04-15 16:34:19 +1000

    Tests sidebar exists

---
+++ app/src/Components/Sidebar/Sidebar.test.js
@@ -0,0 +1,12 @@
+
+import Sidebar from './Sidebar';
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+
+// Sidebar exists
+test("Sidebar to exist", () => {
+    expect(Sidebar).toBeDefined();
+}) 
+
+
+

6d572b9cab96f1384f09b12c105375a32cfbb28b
Kaidin Jackson <s3604690@student.rmit.edu.au>
2021-04-22 10:47:34 +1000

    Previous test was implemented incorrectly. It was failing to change the values of the form giving it a false positive. This test correctly changes values and checks for them.

--- app/src/Components/Signup/Signup.test.js
+++ app/src/Components/Signup/Signup.test.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Signup from './Signup';
-import {render, fireEvent, cleanup} from '@testing-library/react';
+import {render, fireEvent, cleanup, getByText} from '@testing-library/react';
 
 afterEach(cleanup)
 
-// testing state changes when typing into username and password field
-it('Inputting text into username and password field updates the state', () => {
-    const { getByPlaceholderText } = render(<Signup />);
+describe('Testing Signup Page input', () => {
+    test('Inputting text into username and password field updates the state', () => {
+        const { getByPlaceholderText, getByLabelText } = render(<Signup />);
 
-    expect(getByPlaceholderText(/Username/i).textContent).toBe("");
+        const usernameInputField = getByPlaceholderText(/Username/i);
+        const passwordInputField = getByPlaceholderText(/Password/i);
+        
+        fireEvent.change(usernameInputField, { target: { value: 'test' } });
+        fireEvent.change(passwordInputField, { target: { value: 'pass' } });
 
-    fireEvent.change(getByPlaceholderText(/Username/i), {target: {value: 'Text' } } )
-
-    expect(getByPlaceholderText(/Username/i).textContent).toBeDefined();
-
-    expect(getByPlaceholderText(/Password/i).textContent).toBe("");
-
-    fireEvent.change(getByPlaceholderText(/Password/i), {target: {value: 'Text' } } )
-
-    expect(getByPlaceholderText(/Password/i).textContent).toBeDefined();
-
-})
+        expect(usernameInputField.value).toEqual('test');
+        expect(passwordInputField.value).toEqual('pass');
+    });
+});
 

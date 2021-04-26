779d0fa6f69cb381163f9240d8d2085c87e8efbb
Kaidin Jackson <s3604690@student.rmit.edu.au>
2021-04-26 16:48:55 +1000

    Test the page renders all elements

--- app/src/Components/Signup.test.js
+++ app/src/Components/Signup.test.js
@@ -5,12 +5,33 @@ import {render, fireEvent, cleanup, getByText} from '@testing-library/react';
 
 afterEach(cleanup)
 
+// Testing that page renders with all elements
+describe('Testing that the elements of the page render', () => {
+    test('Text and image render', () => {
+
+        const root = document.createElement("div");
+        ReactDOM.render(<Signup />, root);
+        const { getByText, getByAltText, getByPlaceholderText } = render(<Signup />, root);
+        
+        getByText('Register to NotCord');
+        getByText('Already have an account?');
+        getByText('Login Here');
+
+        getByAltText('NotCord Logo');
+
+        getByPlaceholderText('Username');
+        getByPlaceholderText('Password');
+        getByPlaceholderText('Password Confirm');
+    });
+});
+
+// Tests the state changes when user inputs values into text boxes
 describe('Testing Signup Page input', () => {
     test('Inputting text into username and password field updates the state', () => {
         const { getByPlaceholderText } = render(<Signup />);
 
         const usernameInputField = getByPlaceholderText(/Username/i);
-        const passwordInputField = getByPlaceholderText(/Passwords/i);
+        const passwordInputField = getByPlaceholderText('Password');
         const passwordConfirmInputField = getByPlaceholderText(/Password Confirm/i);
         
         fireEvent.change(usernameInputField, { target: { value: 'test' } });

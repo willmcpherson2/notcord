08d720fd2f5b38b9fb8e3c32da736184041830e7
Stokeometry <s3431547@student.rmit.edu.au>
2021-05-28 23:39:52 +1000

    add missing error tests for remove user from group

--- src/test.rs
+++ src/test.rs
@@ -1470,6 +1470,275 @@ fn remove_user_from_group_success() {
 }
 
 #[test]
+fn remove_user_from_group_not_logged_in() {
+    let (client, _) = setup!();   
+
+    let message = client
+        .post("/remove_user_from_group")
+        .header(ContentType::JSON)
+        .body(
+            "{
+                    \"username\":\"test_user02\",
+                    \"group_name\":\"test_group01\"
+                }",
+        );
+
+    let mut response = message.dispatch();
+
+    assert_eq!(
+        response.body_string(),
+        Some(serde_json::to_string(&Err::NotLoggedIn).unwrap())
+    );
+}
+
+#[test]
+fn remove_user_from_group_group_doesnt_exist() {
+    let (client, _) = setup!();
+
+    client
+        .post("/signup")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user01\",   
+            \"password\":\"test_hash01\"
+            }",
+        )
+        .dispatch();
+    client
+        .post("/signup")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user02\",   
+            \"password\":\"test_hash02\"
+            }",
+        )
+        .dispatch();
+    client
+        .post("/login")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user01\",   
+            \"password\":\"test_hash01\"
+        }",
+        )
+        .dispatch();
+
+    let message = client
+        .post("/remove_user_from_group")
+        .header(ContentType::JSON)
+        .body(
+            "{
+                    \"username\":\"test_user02\",
+                    \"group_name\":\"test_group01\"
+                }",
+        );
+
+    let mut response = message.dispatch();
+
+    assert_eq!(
+        response.body_string(),
+        Some(serde_json::to_string(&Err::GroupDoesNotExist).unwrap())
+    );
+}
+
+#[test]
+fn remove_user_from_group_permission_denied() {
+    let (client, _) = setup!();
+
+    client
+        .post("/signup")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user01\",   
+            \"password\":\"test_hash01\"
+            }",
+        )
+        .dispatch();
+    client
+        .post("/signup")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user02\",   
+            \"password\":\"test_hash02\"
+            }",
+        )
+        .dispatch();
+    client
+        .post("/login")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user01\",   
+            \"password\":\"test_hash01\"
+        }",
+        )
+        .dispatch();
+    client
+        .post("/add_group")
+        .header(ContentType::JSON)
+        .body("\"test_group01\"")
+        .dispatch();
+    client
+        .post("/invite_user_to_group")
+        .header(ContentType::JSON)
+        .body(
+            "{
+                    \"username\":\"test_user02\",
+                    \"group_name\":\"test_group01\"
+                }",
+        )
+        .dispatch();
+    client
+        .post("/login")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user02\",   
+            \"password\":\"test_hash02\"
+        }",
+        )
+        .dispatch();
+    client
+        .post("/process_group_invite")
+        .header(ContentType::JSON)
+        .body(
+            "{
+                \"group_name\":\"test_group01\",
+                \"response\":true
+                }",
+        )
+        .dispatch();
+
+    let message = client
+        .post("/remove_user_from_group")
+        .header(ContentType::JSON)
+        .body(
+            "{
+                    \"username\":\"test_user01\",
+                    \"group_name\":\"test_group01\"
+                }",
+        );
+
+    let mut response = message.dispatch();
+
+    assert_eq!(
+        response.body_string(),
+        Some(serde_json::to_string(&Err::PermissionDenied).unwrap())
+    );
+}
+
+#[test]
+fn remove_user_from_group_user_doesnt_exist() {
+    let (client, _) = setup!();
+
+    client
+        .post("/signup")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user01\",   
+            \"password\":\"test_hash01\"
+            }",
+        )
+        .dispatch();
+    client
+        .post("/login")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user01\",   
+            \"password\":\"test_hash01\"
+        }",
+        )
+        .dispatch();
+    client
+        .post("/add_group")
+        .header(ContentType::JSON)
+        .body("\"test_group01\"")
+        .dispatch();
+
+    let message = client
+        .post("/remove_user_from_group")
+        .header(ContentType::JSON)
+        .body(
+            "{
+                    \"username\":\"test_user02\",
+                    \"group_name\":\"test_group01\"
+                }",
+        );
+
+    let mut response = message.dispatch();
+
+    assert_eq!(
+        response.body_string(),
+        Some(serde_json::to_string(&Err::UserDoesNotExist).unwrap())
+    );
+}
+
+#[test]
+fn remove_user_from_group_user_not_in_group() {
+    let (client, _) = setup!();
+
+    client
+        .post("/signup")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user01\",   
+            \"password\":\"test_hash01\"
+            }",
+        )
+        .dispatch();
+    client
+        .post("/signup")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user02\",   
+            \"password\":\"test_hash02\"
+            }",
+        )
+        .dispatch();
+    client
+        .post("/login")
+        .header(ContentType::JSON)
+        .body(
+            "{
+            \"username\":\"test_user01\",   
+            \"password\":\"test_hash01\"
+        }",
+        )
+        .dispatch();
+    client
+        .post("/add_group")
+        .header(ContentType::JSON)
+        .body("\"test_group01\"")
+        .dispatch();
+
+    let message = client
+        .post("/remove_user_from_group")
+        .header(ContentType::JSON)
+        .body(
+            "{
+                    \"username\":\"test_user02\",
+                    \"group_name\":\"test_group01\"
+                }",
+        );
+
+    let mut response = message.dispatch();
+
+    assert_eq!(
+        response.body_string(),
+        Some(serde_json::to_string(&Err::UserNotInGroup).unwrap())
+    );
+}
+
+#[test]
 fn get_users_in_group_success() {
     let (client, _) = setup!();
 
